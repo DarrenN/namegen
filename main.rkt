8f9c27e5e1dadc175ebd10796cf4a1577021b719
DarrenN <info@v25media.com>
2022-08-28 13:55:26 -0400

    Updates main to generate release style names

--- main.rkt
+++ main.rkt
@@ -8,27 +8,39 @@
            racket/fasl
            racket/runtime-path)
 
+  (define-runtime-path adjectives-path "data/adjectives.fasl")
   (define-runtime-path cities-path "data/cities.fasl")
   (define-runtime-path colors-path "data/colors.fasl")
   (define-runtime-path elements-path "data/periodic-table.fasl")
 
+  (define adjectives (call-with-input-file adjectives-path fasl->s-exp))
   (define cities (call-with-input-file cities-path fasl->s-exp))
   (define colors (call-with-input-file colors-path fasl->s-exp))
   (define elements (call-with-input-file elements-path fasl->s-exp))
 
   (define use-color (make-parameter #f))
+  (define use-release (make-parameter #f))
 
   (define (get-random-item vec)
     (vector-ref vec (random 0 (vector-length vec))))
-  
+
+  (define (get-random-list-item ls)
+    (list-ref ls (random 0 (length ls))))
+
+  (define (get-adjective-item s as)
+    (get-random-list-item (hash-ref as (char-downcase (string-ref s 0)))))
+
   (command-line
    #:program "namegen"
    #:once-each
    [("-c" "--color") "Use colors instead of cities" (use-color #t)]
+   [("-r" "--release") "Generate Ubuntu style release name (ex: chubbyCesium)" (use-release #t)]
    #:args ()
-   (displayln (format
-               "~a~a"
-               (get-random-item elements)
-               (if (use-color)
-                   (get-random-item colors)
-                   (get-random-item cities))))))
+   (let ([element (get-random-item elements)])
+     (displayln (format
+                 "~a~a"
+                 (cond [(use-release) (get-adjective-item element adjectives)]
+                       [else element])
+                 (cond [(use-color) (get-random-item colors)]
+                       [(use-release) element]
+                       [else (get-random-item cities)]))))))
